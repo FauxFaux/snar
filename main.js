d7a8d4f1fcb206c0a1be7fe5d6f3796f90ccab40
Chris West (Faux) <git@goeswhere.com>
2014-11-02 00:48:45 +0000

    points find their best roads

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
+function sqr(x) {
+    return x * x
+}
+
+function distSquared(p1, p2) {
+    return sqr(p1.x - p2.x) + sqr(p1.y - p2.y)
+}
+
+function distToSegmentSquared(point, start, end) {
+    var l2 = distSquared(start, end);
+    if (l2 == 0) return distSquared(point, start);
+    var t = ((point.x - start.x) * (end.x - start.x) + (point.y - start.y) * (end.y - start.y)) / l2;
+    if (t < 0) return distSquared(point, start);
+    if (t > 1) return distSquared(point, end);
+    return distSquared(point, {
+        x: start.x + t * (end.x - start.x),
+        y: start.y + t * (end.y - start.y)
+    });
+}
+
 function seg(map, x1, y1, x2, y2) {
     var points = [
         new google.maps.LatLng(x1, y1),
         new google.maps.LatLng(x2, y2)];
 
-    new google.maps.Polyline({
+    return new google.maps.Polyline({
         path: points,
         geodesic: true,
         strokeColor: 'black',
@@ -23,9 +43,19 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
+    var roads = [];
+
     $.get('http://localhost:9000/', function (x) {
         x.forEach(function (item) {
-            seg(map, item[0][0], item[0][1], item[1][0], item[1][1]);
+            var x0 = item[0][0];
+            var x1 = item[1][0];
+            var y0 = item[0][1];
+            var y1 = item[1][1];
+            roads.push({
+                'start': {'x': x0, 'y': y0},
+                'end': {'x': x1, 'y': y1},
+                'seg': seg(map, x0, y0, x1, y1)
+            });
         });
     });
 
@@ -46,6 +76,22 @@ function initialize() {
             seek.attr('max', points.length);
             seek.on('input', function () {
                 circle.set('center', points[this.value]);
+            });
+
+            points.forEach(function (point) {
+                var loc = {'x': point.lat(), 'y': point.lng()};
+                var best = 99999999;
+                var bestRoad = undefined;
+                roads.forEach(function(road) {
+                    var curr = distToSegmentSquared(loc, road.start, road.end);
+                    if (curr < best) {
+                        best = curr;
+                        bestRoad = road;
+                    }
+                });
+                bestRoad.seg.set('strokeColor', 'red');
+                bestRoad.seg.set('strokeWeight', 2);
+                point.score = best;
             });
 
             new google.maps.Polyline({
